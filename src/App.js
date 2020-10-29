27caba02cebffdcbd93e6db853b3cfae229d979e
ali Hussain <ali.talha3357@gmail.com>
2020-10-29 15:53:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome all
+          welcome everyone
           
         </p>
         <a
