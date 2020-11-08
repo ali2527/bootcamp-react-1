6f7823e4e8dd8912806d199b12b321ab6c05fd5a
ali Hussain <ali.talha3357@gmail.com>
2020-11-08 19:42:37 +0500

    Update Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -3,7 +3,7 @@
 function Dinner(props){
     return(
         <div>
-            <h1>today for dinner i am having {props.item}</h1>
+            <h1>today for dinner i am having {props.item} and {props.desert}</h1>
         </div>
     );
 }
