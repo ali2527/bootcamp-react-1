d85093681d14991f66b5a30bcea8f93eb7ec3692
ali Hussain <ali.talha3357@gmail.com>
2020-11-11 00:42:16 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Dinner from './Dinner'
-import { useState } from 'react';
+
+
 
 function App() {
+ 
+  let [count,setCount] = useState(0);
+
   return (
     <div className="App">
-      <Dinner item="biryani" desert="kheer"/>
+      <Dinner item="biryani" desert={() => setCount()} />
      
     </div>
-  );
-}
+    );
+  }
 
 export default App;
