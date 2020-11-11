7a8d8fb4de6d38e4b5436bb39162327d468f5146
ali Hussain <ali.talha3357@gmail.com>
2020-11-11 23:57:45 +0500

    Update Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -4,9 +4,10 @@ import React from "react";
 function Dinner(props){
     return(
         <div>
-            <h1>today for dinner i am having {props.item} </h1>
-            <h1>value of counter is {props.count}</h1>
             
+            <h1>Counter </h1><br/>
+            <h1>{props.count}</h1>
+
         </div>
     );
 }
