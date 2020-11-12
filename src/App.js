61418c304ff2f154634e148c0f2b5a27c74ac696
ali Hussain <ali.talha3357@gmail.com>
2020-11-13 00:53:07 +0500

    begin new expense tracker

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Parent from "./parent";
-import CounterContexvar from "./counterContex";
+
 
 function App() {
-  let [Ismorning, setMorning] = useState(false);
-  let counterState = useState(0); //[counter,setCounter]
-
   return (
-    <CounterContexvar.Provider value={counterState}>
-      <div className={`App ${Ismorning ? "night" : ""}`}>
-        <Parent />
-        <button onClick={() => setMorning(!Ismorning)}> Switch</button>
-      </div>
-    </CounterContexvar.Provider>
+    <div>
+
+    </div>
   );
 }
 
--- src/counterContex.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const CounterContexvar = createContext("ali");
-
-export default CounterContexvar;
